2f53a18677abe49926eb7a71c79554bc2319073b
Tim-Irwin <tmthyirwin@gmail.com>
2025-12-24 10:46:21 -0600

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+
+   public static int calculatePunchDamage(int strength){
+       return strength * 2;
+   }
+
+   public static int calculateSpecialAttackDamage(int strength, String attackType){
+       int damage = strength;
+       if(attackType.equals("Fire Punch")){
+           damage = damage * 3;
+       } else if(attackType.equals("Ice Blast")){
+           damage = damage * 2;
+       }
+       return damage;
+   }
+
+
+    public static void main(String[] args) {
+        int villainHealth = 100;
+        int punchStrength = 20;
+        String attackType = "Fire Punch";
+
+        int damageByPunch = calculatePunchDamage(punchStrength);
+        int damageBySpecialAttack = calculateSpecialAttackDamage(punchStrength, attackType);
+
+        System.out.println("Damage by punch: " + damageByPunch + "\nDamage by special attack: "
+            + damageBySpecialAttack);
+    }
 }
